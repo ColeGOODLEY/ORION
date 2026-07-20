0ff9fec6dba605d7c213fba40ce96221e20db9ef
ColeGOODLEY <colehgoodley@gmail.com>
2026-07-20 12:38:44 -0500

    Create ORION memory system

--- memory.js
+++ memory.js
@@ -1,1 +1,21 @@
+// ORION MEMORY SYSTEM v0.1
 
+function saveMemory(key, value){
+
+localStorage.setItem(key, value);
+
+}
+
+
+function recallMemory(key){
+
+return localStorage.getItem(key);
+
+}
+
+
+function clearMemory(key){
+
+localStorage.removeItem(key);
+
+}
