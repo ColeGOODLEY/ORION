5127a58fd49d1ce3756445feb4bb88cb38019440
ColeGOODLEY <colehgoodley@gmail.com>
2026-07-20 11:38:07 -0500

    Move ORION logic to script.js
    Added a function to execute commands and display responses.

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function executeORION(){
+
+let command =
+document.getElementById("command").value;
+
+
+let response =
+document.getElementById("response");
+
+
+if(command == ""){
+
+response.innerHTML =
+"Command required.";
+
+}
+
+else {
+
+
+response.innerHTML =
+"Command received.<br><br>" +
+"Analyzing: " + command +
+"<br><br>" +
+"ORION is standing by.";
+
+
+}
+
+
+}
+
 
