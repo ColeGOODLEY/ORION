a195630fc6805f5a4be47cd65178ce6970be1f93
ColeGOODLEY <colehgoodley@gmail.com>
2026-07-20 14:01:22 -0500

    Fix ORION script structure

--- script.js
+++ script.js
@@ -104,57 +104,7 @@ memory;
 }
 
 
-// ORION MEMORY SYSTEM v0.3
 
-function saveMemory(category, value){
-
-let memories =
-JSON.parse(localStorage.getItem("orion_memory")) || {};
-
-
-if(!memories[category]){
-
-memories[category] = [];
-
-}
-
-
-memories[category].push(value);
-
-
-localStorage.setItem(
-"orion_memory",
-JSON.stringify(memories)
-);
-
-}
-
-
-function recallMemory(category){
-
-let memories =
-JSON.parse(localStorage.getItem("orion_memory")) || {};
-
-
-return memories[category];
-
-}
-
-
-function getAllMemories(){
-
-return JSON.parse(
-localStorage.getItem("orion_memory")
-) || {};
-
-}
-
-
-function clearMemory(){
-
-localStorage.removeItem("orion_memory");
-
-}
 else if(command == "joke"){
 
 response.innerHTML =
