7818dadd187cb63e7a8b6d790424f5c48b47bdac
ColeGOODLEY <colehgoodley@gmail.com>
2026-07-25 03:07:13 -0500

    Check if ORION is defined before processing

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function executeORION() {
 
     const output = document.getElementById("response");
 
+    if (typeof ORION === "undefined") {
+
+        output.innerHTML =
+        "ERROR: ORION Core is not loaded.";
+
+        return;
+
+    }
+
+
     output.innerHTML =
-        ORION.process("TEST");
+    "SUCCESS: ORION Core is loaded.";
 
 }
