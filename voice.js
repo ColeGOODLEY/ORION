836befc69e9673b393f5da47cd1b6288843eb51f
ColeGOODLEY <colehgoodley@gmail.com>
2026-07-31 22:30:18 -0500

    Update pitch and preferred voice in voice.js

--- voice.js
+++ voice.js
@@ -34,7 +34,7 @@ settings:{
 
 rate:0.88,
 
-pitch:0.65,
+pitch:0.80,
 
 volume:1,
 
@@ -293,7 +293,7 @@ return;
 let preferredVoice =
 voices.find(voice =>
 
-voice.name === "Chrome OS US English 6"
+voice.name === "Chrome OS US English 4"
 
 );
 
